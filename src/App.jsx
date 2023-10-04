ae0c5b417ae94337b4479f7470f56e881f92b4c8
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-10-04 16:34:24 +0300

    added navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ import { Layout, Login, Register } from "./components";
 import { empty, login, register } from "./data/constants/routes";
 
 const router = createBrowserRouter([
-  { path: empty, element: <Layout /> },
-  { path: login, element: <Login /> },
-  { path: register, element: <Register /> },
+  {
+    path: empty,
+    element: <Layout />,
+    children: [
+      { path: login, element: <Login /> },
+      { path: register, element: <Register /> },
+      { path: empty, element: <h1>Home</h1> },
+    ],
+  },
 ]);
 
 function App() {
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Login from "./Login";
 import Register from "./Register";
 import Layout from "./Layout";
+import Navbar from "./Navbar";
 
-export { Layout, Login, Register };
+export { Layout, Login, Register, Navbar };
