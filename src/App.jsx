53f6ccdf623691c9a849e38f26d820b55a865c19
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-10-04 14:46:31 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/global.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+      <h1>Fresh Cart</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,27 @@
+import banner from "./banner-4.jpeg";
+import blogImg1 from "./blog-img-1.jpeg";
+import blogImg2 from "./blog-img-2.jpeg";
+import sliderImage1 from "./slider-image-1.jpeg";
+import sliderImage2 from "./slider-image-2.jpeg";
+import sliderImage3 from "./slider-image-3.jpeg";
+import groceryBanner2 from "./grocery-banner-2.jpeg";
+
+import groceryBanner1 from "./grocery-banner.png";
+
+import error from "./error.svg";
+import logo from "./freshcart-logo.svg";
+import lightPattern from "./light-patten.svg";
+
+export {
+  logo,
+  error,
+  banner,
+  blogImg1,
+  blogImg2,
+  lightPattern,
+  sliderImage1,
+  sliderImage2,
+  sliderImage3,
+  groceryBanner2,
+  groceryBanner1,
+};
