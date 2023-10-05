8c79d4ce8310a14bbe539eb63927cefdf9d2a4e6
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-10-05 18:11:27 +0300

    finished register cycle

--- src/data/constants/network.js
+++ src/data/constants/network.js
@@ -1,4 +1,5 @@
 const baseUrl = "https://ecommerce.routemisr.com";
 const login = `${baseUrl}/api/v1/auth/signin`;
+const register = `${baseUrl}/api/v1/auth/signup`;
 
-export { login as loginUrl };
+export { login as loginUrl, register as registerUrl };
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,10 +1,23 @@
 import * as Yup from "yup";
 
+const name = Yup.string().required("Must enter a name");
 const email = Yup.string()
   .required("Must enter an email")
   .email("Must be a valid email");
+const phone = Yup.string()
+  .required("Muster enter a phone number")
+  .matches(/^(\+2)?01[0125][0-9]{8}$/, "Must enter an Egyptian number");
 const password = Yup.string()
   .required("Must enter a password")
   .min(6, "Must be at least 6 characters");
+const rePassword = Yup.string()
+  .required("Must enter the password")
+  .oneOf([Yup.ref("password")], "Must be equal to password");
 
-export { email as emailValidator, password as passwordValidator };
+export {
+  name as nameValidator,
+  email as emailValidator,
+  phone as phoneValidator,
+  password as passwordValidator,
+  rePassword as rePasswordValidator,
+};
